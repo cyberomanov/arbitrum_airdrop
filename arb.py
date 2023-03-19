6b7f55136666c09c26983eaa8e7326b00c7903f9
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2023-03-19 15:02:01 +0400

    db auto find added.

--- arb.py
+++ arb.py
@@ -1,10 +1,7 @@
 import json
+import re
 
 import requests
-
-import re
-get_db_value = requests.get("https://arbitrum.foundation/eligibility?address=0x0000000000000000000000000000000000000000")
-DB_LINK = re.search('buildId":"(.{21})"', get_db_value.text).group(1)
 
 
 def get_addresses(path: str = 'address.txt') -> list:
@@ -13,9 +10,9 @@ def get_addresses(path: str = 'address.txt') -> list:
     return [line.rstrip() for line in lines]
 
 
-def get_token(address: str) -> float:
-    response = session.get(url=f"https://arbitrum.foundation/_next/data/{DB_LINK}/eligibility.json?"
-                               f"address={address.lower()}")
+def get_token(address: str, db_link: str) -> float:
+    response = requests.get(url=f"https://arbitrum.foundation/_next/data/{db_link}/eligibility.json?"
+                                f"address={address.lower()}")
     result = json.loads(response.content)['pageProps']
     if result['isEligible']:
         return result['eligibility']['tokens']
@@ -23,11 +20,21 @@ def get_token(address: str) -> float:
         return 0.0
 
 
-if __name__ == '__main__':
+def get_db_link() -> str:
+    response = requests.get(
+        "https://arbitrum.foundation/eligibility?address=0x0000000000000000000000000000000000000000")
+    return re.search('buildId":"(.{21})"', response.text).group(1)
+
+
+def main():
     total_amount = 0.0
-    session = requests.session()
+    db_link = get_db_link()
     for address in get_addresses():
-        amount = get_token(address=address)
+        amount = get_token(address=address, db_link=db_link)
         total_amount += amount
         print(f"{address}: {amount}.")
     print(f"total: {total_amount}.")
+
+
+if __name__ == '__main__':
+    main()
